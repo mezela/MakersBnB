f9d5ee1ba339a02b441e0b26501470961a906a2a
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-10-21 16:10:49 +0100

    Refactored to include production database

--- lib/user.rb
+++ lib/user.rb
@@ -11,16 +11,25 @@ class User
   end
 
   def self.create(username: ,password: ,email:)
-    connection = PG.connect(dbname: 'makersbnb_test')
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
     result = connection.exec("INSERT INTO users (username, email, password) VALUES('#{username}', '#{email}', '#{password}') RETURNING id, username, email;")
     User.new(id: result[0]['id'], username: result[0]['username'], email: result[0]['email'])
   end
 
   def self.all
-    connection = PG.connect(dbname: 'makersbnb_test')
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
     users = connection.exec("SELECT * FROM users;")
     users.map do |user|
       User.new(id: user['id'], username: user['username'] , email: user['email'])
     end
   end
+  
 end
