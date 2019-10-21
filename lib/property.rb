fd12dca084a33b039ae1d74729bb97a4cc27e731
AlecDWalker <walkeralec@hotmail.com>
2019-10-21 16:24:06 +0100

    refactored

--- lib/property.rb
+++ lib/property.rb
@@ -6,14 +6,14 @@ class Property
   attr_reader :id, :ownerID, :title, :description, :picture, :ppn, :start_date, :end_date
 
   def initialize(
-    id: id,
-    ownerID: ownerID,
-    title: title,
-    description: description,
-    picture: picture,
-    ppn: ppn,
-    start_date: start_date,
-    end_date: end_date
+    id:,
+    ownerID:,
+    title:,
+    description:,
+    picture:,
+    ppn:,
+    start_date:,
+    end_date:
   )
     @id = id
     @ownerID = ownerID
@@ -45,13 +45,13 @@ class Property
   end
 
   def self.add(
-    ownerID: ownerID,
-    title: title,
-    description: description,
-    picture: picture,
-    ppn: ppn,
-    start_date: start_date,
-    end_date: end_date)
+    ownerID:,
+    title:,
+    description:,
+    picture:,
+    ppn:,
+    start_date:,
+    end_date:)
     connection = PG.connect(dbname: which_database)
     sql = "INSERT INTO properties (ownerID, title, description, picture, ppn, start_available_date, end_available_date) VALUES('#{ownerID}','#{title}','#{description}','#{picture}','#{ppn}','#{start_date}','#{end_date}');"
     connection.exec(sql)
