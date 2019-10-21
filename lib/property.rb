c30e422c90d8b3f2519f58f10e0c252ed6e830be
AlecDWalker <walkeralec@hotmail.com>
2019-10-21 16:07:57 +0100

    add function working

--- lib/property.rb
+++ lib/property.rb
@@ -3,10 +3,11 @@ require 'pg'
 
 class Property
 
-  attr_reader :id, :title, :description, :picture, :ppn, :start_date, :end_date
+  attr_reader :id, :ownerID, :title, :description, :picture, :ppn, :start_date, :end_date
 
   def initialize(
     id: id,
+    ownerID: ownerID,
     title: title,
     description: description,
     picture: picture,
@@ -15,6 +16,7 @@ class Property
     end_date: end_date
   )
     @id = id
+    @ownerID = ownerID
     @title = title
     @description = description
     @picture = picture
@@ -24,21 +26,34 @@ class Property
   end
 
   def self.view_all
-    @connection = PG.connect(dbname: which_database)
+    connection = PG.connect(dbname: which_database)
     sql = 'SELECT * FROM properties'
-    result = @connection.exec(sql)
+    result = connection.exec(sql)
     @results = result.map do |property|
       Property.new(
         id: property['id'],
+        ownerID: property['ownerID'],
         title: property['title'],
         description: property['description'],
         picture: property['picture'],
         ppn: property['ppn'],
-        start_date: property['start_date'],
-        end_date: property['end_date']
+        start_date: property['start_available_date'],
+        end_date: property['end_available_date']
       )
-  end
+    end
   @results
-end
+  end
 
+  def self.add(
+    ownerID: ownerID,
+    title: title,
+    description: description,
+    picture: picture,
+    ppn: ppn,
+    start_date: start_date,
+    end_date: end_date)
+    connection = PG.connect(dbname: which_database)
+    sql = "INSERT INTO properties (ownerID, title, description, picture, ppn, start_available_date, end_available_date) VALUES('#{ownerID}','#{title}','#{description}','#{picture}','#{ppn}','#{start_date}','#{end_date}');"
+    connection.exec(sql)
+  end
 end
